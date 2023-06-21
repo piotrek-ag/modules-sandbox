75dd21eb3c11caa35cc2b80f9caa3d7349a8616a
piotrek-ag <piotr.agier@gmail.com>
2023-06-21 15:34:50 +0200

    Initial commit, empty Spring Boot project

---
+++ src/main/java/com/twise/modulessandbox/ModulesSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.twise.modulessandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModulesSandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModulesSandboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/twise/modulessandbox/ModulesSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twise.modulessandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModulesSandboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
